d921ce7c2c4f7c44690dd86936a6c58c673f8940
Wes Bos <wesbos@gmail.com>
2019-10-21 14:46:46 -0400

    update intl methods

--- intl.js
+++ intl.js
@@ -11,9 +11,23 @@ new Intl.DateTimeFormat('en-US', { dateStyle: 'short' }).format(now);
 // 10/15/19
 new Intl.DateTimeFormat('fr-CA', { dateStyle: 'long' }).format(now);
 // 15 octobre 2019
+const options = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: false,
+  timeZone: 'America/Los_Angeles',
+};
+new Intl.DateTimeFormat(options).format(now);
 
 // new Intl.RelativeTimeFormat
-const relative = new Intl.RelativeTimeFormat('en', { style: 'long', numeric: 'auto' });
+const relative = new Intl.RelativeTimeFormat('en', {
+  style: 'long',
+  numeric: 'auto',
+});
 
 relative.format(3, 'day'); // in 3 days
 relative.format(7, 'day'); // in 7 days
@@ -28,7 +42,10 @@ relative.format(-20, 'second'); // 20 seconds ago
 relative.format(-1, 'second'); // 1 second ago
 relative.format(10, 'second'); // in 10 seconds
 
-var relativeFR = new Intl.RelativeTimeFormat('fr', { style: 'long', numeric: 'auto' });
+const relativeFR = new Intl.RelativeTimeFormat('fr', {
+  style: 'long',
+  numeric: 'auto',
+});
 
 relativeFR.format(3, 'day'); // dans 3 jours
 relativeFR.format(7, 'day'); // dans 7 jours
@@ -52,21 +69,35 @@ new Intl.NumberFormat('fr-FR').format(872384.32);
 // >> 872 384,32
 
 // units
-new Intl.NumberFormat('en-GB', { style: 'unit', unit: 'liter'}).format(16);
+new Intl.NumberFormat('en-GB', { style: 'unit', unit: 'liter' }).format(16);
 // >> 16 l
-new Intl.NumberFormat('en-GB', { style: 'unit', unit: 'liter', unitDisplay: "long" }).format(16);
+new Intl.NumberFormat('en-GB', {
+  style: 'unit',
+  unit: 'liter',
+  unitDisplay: 'long',
+}).format(16);
 // >> 16 litres
 
 // currency
-new Intl.NumberFormat('en-US', { style: 'currency', currency: 'EUR' }).format(1000);
+new Intl.NumberFormat('en-US', { style: 'currency', currency: 'EUR' }).format(
+  1000
+);
 // >> €1,000.00
-new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(1000);
+new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
+  1000
+);
 // >> 1.000,00 €
-new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(1000);
+new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(
+  1000
+);
 // >> 1.000,00 $
-new Intl.NumberFormat('en-US', { style: 'currency', currency: 'CAD' }).format(12.32);
+new Intl.NumberFormat('en-US', { style: 'currency', currency: 'CAD' }).format(
+  12.32
+);
 // >> CA$12.32
-new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(12.32);
+new Intl.NumberFormat('en-CA', { style: 'currency', currency: 'CAD' }).format(
+  12.32
+);
 // >> $12.32
 
 // Intl.ListFormat
@@ -74,43 +105,51 @@ const foods = ['Pizza', 'Tacos', 'Burgers'];
 
 new Intl.ListFormat('en', { style: 'long', type: 'conjunction' }).format(foods);
 // Pizza, Tacos, and Burgers
-new Intl.ListFormat('en', { style: 'short', type: 'conjunction' }).format(foods);
+new Intl.ListFormat('en', { style: 'short', type: 'conjunction' }).format(
+  foods
+);
 // Pizza, Tacos, & Burgers
 new Intl.ListFormat('en', { style: 'long', type: 'disjunction' }).format(foods);
 // Pizza, Tacos, or Burgers
 new Intl.ListFormat('en', { style: 'long', type: 'unit' }).format(foods);
 // Pizza, Tacos, Burgers
-new Intl.ListFormat('fr-CA', { style: 'long', type: 'conjunction' }).format(foods);
+new Intl.ListFormat('fr-CA', { style: 'long', type: 'conjunction' }).format(
+  foods
+);
 // Pizza, Tacos et Burgers
-new Intl.ListFormat('fr-CA', { style: 'long', type: 'disjunction' }).format(foods);
+new Intl.ListFormat('fr-CA', { style: 'long', type: 'disjunction' }).format(
+  foods
+);
 // Pizza, Tacos ou Burgers
-new Intl.ListFormat('th-thai', { style: 'long', type: 'disjunction' }).format(foods);
+new Intl.ListFormat('th-thai', { style: 'long', type: 'disjunction' }).format(
+  foods
+);
 // Pizza, Tacos หรือ Burgers
-new Intl.ListFormat('th-thai', { style: 'long', type: 'conjunction' }).format(foods);
+new Intl.ListFormat('th-thai', { style: 'long', type: 'conjunction' }).format(
+  foods
+);
 // Pizza Tacos และBurgers
-
 
 // Intl.PluralRules
 
 function stndrdth(number) {
   const pr = new Intl.PluralRules('en-US', { type: 'ordinal' });
-  let ordinal;
-  switch (pr.select(number)) {
-    case 'one':
-      ordinal = 'st';
-      break;
-    case 'two':
-      ordinal = 'nd';
-      break;
-    case 'few':
-      ordinal = 'rd';
-      break;
-    default:
-      ordinal = 'th';
-      break;
-  }
-  return `${number}${ordinal}`;
+  // pr.select(number) returns on of the following:
+  const ordinals = {
+    one: 'st',
+    two: 'nd',
+    few: 'rd',
+    many: 'th',
+    zero: 'th',
+    other: 'th',
+  };
+  return `${number}${ordinals[pr.select(number)]}`;
 }
+
+stndrdth(1); // 1st
+stndrdth(2); // 2nd
+stndrdth(3); // 3rd
+stndrdth(4); // 4th
 
 const dates = Array.from({ length: 31 }, (_, number) => stndrdth(number + 1));
 // ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
@@ -124,3 +163,9 @@ new Intl.Collator('en-US').compare('a', 'A'); // -1 BEFORE
 new Intl.Collator('en-US').compare('a', 'á'); // -1 BEFORE
 new Intl.Collator('en-US', { sensitivity: 'base' }).compare('a', 'A'); // 0 SAME
 new Intl.Collator('en-US', { sensitivity: 'base' }).compare('a', 'ä'); // 0 SAME
+
+
+
+const options = { weekday: 'long', hour: 'numeric' };
+new Intl.DateTimeFormat('en-US', options).format(new Date())
+// "Friday, 2 PM"
